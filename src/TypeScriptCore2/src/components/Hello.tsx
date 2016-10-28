72120e32fd67350d2e9fc7e61e4d1c0d99c647d7
ozippy <jeremy@ozippy.com>
2016-10-28 23:18:23 +1100

    before i delete libs

--- src/TypeScriptCore2/src/components/Hello.tsx
+++ src/TypeScriptCore2/src/components/Hello.tsx
@@ -1,3 +1,9 @@
-﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
-// for more information see the following page on the TypeScript wiki:
-// https://github.com/Microsoft/TypeScript/wiki/JSX
+﻿import * as React from "react";
+
+export interface HelloProps { compiler: string; framework: string; }
+
+export class Hello extends React.Component<HelloProps, {}> {
+    render() {
+        return <h1>Hello from {this.props.compiler}and {this.props.framework}!</h1>;
+    }
+}
