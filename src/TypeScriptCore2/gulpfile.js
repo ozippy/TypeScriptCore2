bd147312b520a22bc8529433bf17836e7569a253
ozippy <jeremy@ozippy.com>
2016-10-28 22:37:37 +1100

    was trying to use webpack but that seems un-necessary

--- src/TypeScriptCore2/gulpfile.js
+++ src/TypeScriptCore2/gulpfile.js
@@ -24,56 +24,18 @@ gulp.task("default", function () {
 
 var libs = './wwwroot/lib/';
 
-gulp.task('restore:core-js', function () {
+gulp.task('restore:react-js', function () {
     gulp.src([
-        'node_modules/core-js/client/*.js'
-    ]).pipe(gulp.dest(libs + 'core-js'));
+        'node_modules/react/dist/*.js'
+    ]).pipe(gulp.dest(libs + 'react'));
 });
-gulp.task('restore:zone.js', function () {
+gulp.task('restore:react-dom.js', function () {
     gulp.src([
-        'node_modules/zone.js/dist/*.js'
-    ]).pipe(gulp.dest(libs + 'zone.js'));
-});
-gulp.task('restore:reflect-metadata', function () {
-    gulp.src([
-        'node_modules/reflect-metadata/reflect.js'
-    ]).pipe(gulp.dest(libs + 'reflect-metadata'));
-});
-gulp.task('restore:systemjs', function () {
-    gulp.src([
-        'node_modules/systemjs/dist/*.js'
-    ]).pipe(gulp.dest(libs + 'systemjs'));
-});
-gulp.task('restore:rxjs', function () {
-    gulp.src([
-        'node_modules/rxjs/**/*.js'
-    ]).pipe(gulp.dest(libs + 'rxjs'));
-});
-gulp.task('restore:angular-in-memory-web-api', function () {
-    gulp.src([
-        'node_modules/angular-in-memory-web-api/**/*.js'
-    ]).pipe(gulp.dest(libs + 'angular-in-memory-web-api'));
-});
-
-gulp.task('restore:angular', function () {
-    gulp.src([
-        'node_modules/@angular/**/*.js'
-    ]).pipe(gulp.dest(libs + '@angular'));
-});
-
-gulp.task('restore:bootstrap', function () {
-    gulp.src([
-        'node_modules/bootstrap/dist/**/*.*'
-    ]).pipe(gulp.dest(libs + 'bootstrap'));
+        'node_modules/react-dom/dist/*.js'
+    ]).pipe(gulp.dest(libs + 'react-dom'));
 });
 
 gulp.task('restore', [
-    'restore:core-js',
-    'restore:zone.js',
-    'restore:reflect-metadata',
-    'restore:systemjs',
-    'restore:rxjs',
-    'restore:angular-in-memory-web-api',
-    'restore:angular',
-    'restore:bootstrap'
+    'restore:react-js',
+    'restore:react-dom.js'
 ]);
