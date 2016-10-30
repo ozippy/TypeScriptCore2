e9ce3d039d24aacf68a2a8d38d8d719a1ab06c07
ozippy <jeremy@ozippy.com>
2016-10-30 17:01:00 +1100

    commit for work

---
+++ src/TypeScriptCore2/wwwroot/js/HelloWorld.jsx
@@ -0,0 +1,8 @@
+"use strict";
+const React = require('react');
+class HelloMessage extends React.Component {
+    render() {
+        return <div>Hello {this.props.name}</div>;
+    }
+}
+module.exports = HelloMessage;
--- src/TypeScriptCore2/wwwroot/js/tutorial.jsx
+++ src/TypeScriptCore2/wwwroot/js/tutorial.jsx
@@ -2,8 +2,10 @@
     render: function () {
         return (
             <div className="commentBox">
-                Hello, world! I am a CommentBox.
-      </div>
+                        <h1>Comments</h1>
+
+        
+            </div>
         );
     }
 });
@@ -11,3 +13,36 @@ ReactDOM.render(
     <CommentBox />,
     document.getElementById('content')
 );
+
+var CommentList = React.createClass({
+    render: function () {
+        return (
+      <div className="commentList">
+          Hello, world! I am a CommentList.
+      </div>
+    );
+    }
+});
+
+//var CommentForm = React.createClass({
+//    render: function () {
+//        return (
+//      <div className="commentForm">
+//          Hello, world! I am a CommentForm.
+//      </div>
+//    );
+//    }
+//});
+
+//var Comment = React.createClass({
+//    render: function() {
+//        return (
+//          <div className="comment">
+//            <h2 className="commentAuthor">
+//              {this.props.author}
+//            </h2>
+//        {this.props.children}
+//        </div>
+//      );
+//    }
+//});
